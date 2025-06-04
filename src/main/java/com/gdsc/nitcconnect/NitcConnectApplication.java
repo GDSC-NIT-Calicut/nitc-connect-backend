4e12af43311cf39ce4b001800f33e7fbaa5a1bd1
Rithvik <rithvikreddym2006@gmail.com>
2025-06-04 20:01:11 +0530

    Backend Initialized

---
+++ src/main/java/com/gdsc/nitcconnect/NitcConnectApplication.java
@@ -0,0 +1,23 @@
+package com.gdsc.nitcconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
+
+@SpringBootApplication
+public class NitcConnectApplication {
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+        System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitcConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc/nitcconnect/NitcConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.nitcconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitcConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
